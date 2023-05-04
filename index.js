5c253b6085dab7959af8d43e6f2550b1cd0805cc
danielagarcia <ruizgarciaeslydaniela@gmail.com>
2023-05-03 21:44:03 -0600

    Adivina-el-numero

---
+++ index.js
@@ -0,0 +1,18 @@
+let min= 1;
+ let max= 100;
+  let guess= 50;
+
+  function guessNumber(response){
+
+    if (response === 'menor') {
+        max = guess - 1;
+    }
+    else if (response === 'igual') {
+        alert ('¡Adivine!');
+    }
+    else if (response === 'mayor') {
+        min = guess +1;
+    }
+    guess = Math.floor((max + min)/2);
+    document.getElementById('guess').textContent = guess;
+  }
